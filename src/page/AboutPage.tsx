76f1d7d8511875355a75d86a30c5c74e76a4f10a
aleexvdev <valverdealex25@gmail.com>
2024-04-07 19:16:00 -0500

    fix

--- src/page/AboutPage.tsx
+++ src/page/AboutPage.tsx
@@ -10,17 +10,7 @@ export const AboutPage = ({ title }: AboutPageProps) => {
       <Header title={title} />
       <div className='text-light-gray font-light leading-6 pt-8'>
         <p className='mb-7'>
-          I'm Creative Director and UI/UX Designer from Sydney, Australia, working in web development and print media.
-          I enjoy
-          turning complex problems into simple, beautiful and intuitive designs.
-        </p>
-
-        <p className='mb-7'>
-          My job is to build your website so that it is functional and user-friendly but at the same time attractive.
-          Moreover, I
-          add personal touch to your product and make sure that is eye-catching and easy to use. My aim is to bring
-          across your
-          message and identity in the most creative way. I created web design for many famous brand companies.
+        AboutPage
         </p>
       </div>
     </section>
